15a2b493ca517561a6b24669c13b629e4fcc7f90
Neophitus <npts@o8s2e7hdk5g.com>
2026-08-12 18:57:22 -0300

    chore(version): identify Bucaneiros MKV fork in build info

--- pkg/consts/version.go
+++ pkg/consts/version.go
@@ -1,8 +1,30 @@
 package consts
 
+import "runtime/debug"
+
 // vars below are set by '-X' flag
 var (
-	Version    = "dev"
+	Version    = "0.20.3 (Bucaneiros fork - Send MKV as Video)"
 	Commit     = "unknown"
 	CommitDate = "unknown"
 )
+
+func init() {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return
+	}
+
+	for _, setting := range info.Settings {
+		switch setting.Key {
+		case "vcs.revision":
+			if Commit == "unknown" && len(setting.Value) >= 7 {
+				Commit = setting.Value[:7]
+			}
+		case "vcs.time":
+			if CommitDate == "unknown" {
+				CommitDate = setting.Value
+			}
+		}
+	}
+}
